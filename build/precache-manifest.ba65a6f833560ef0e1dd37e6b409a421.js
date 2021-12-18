2be9336183aafd1fce94c2ea3e447467439d1911
Nipun Hedaoo <nipunhedaoo@gmail.com>
2021-12-19 00:12:32 +0530

    build

---
+++ build/precache-manifest.ba65a6f833560ef0e1dd37e6b409a421.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "952b9b4a863a71b28d296694bba64708",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83b5f9b682940abc170a",
+    "url": "/static/css/main.5740e064.chunk.css"
+  },
+  {
+    "revision": "51ed192b9871cb93ed88",
+    "url": "/static/js/2.42882df1.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.42882df1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83b5f9b682940abc170a",
+    "url": "/static/js/main.6344c44f.chunk.js"
+  },
+  {
+    "revision": "6f549d233488bc7fe5da",
+    "url": "/static/js/runtime-main.dc28e340.js"
+  }
+]);
